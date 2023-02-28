b32a91b13fb5005b0067bdb106dee43221da7f6f
D-Lanzzz <delangtg123@gmail.com~~>
2023-02-28 08:05:07 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+   <?php
+
+echo date('d F Y', strtotime('2022-08-17'));
+
+?>
+
+    <!--
+    This script places a badge on your repl's full-browser view back to your repl's cover
+    page. Try various colors for the theme: dark, light, red, orange, yellow, lime, green,
+    teal, blue, blurple, magenta, pink!
+    -->
+    <script src="https://replit.com/public/js/replit-badge.js" theme="blue" defer></script> 
+  </body>
+</html>
